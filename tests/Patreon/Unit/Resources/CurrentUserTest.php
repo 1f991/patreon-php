a86410c3fb8aeb5f26cfa0766de979663d2d3efb
Samuel Ryan <sam@samryan.co.uk>
2018-04-09 20:17:57 +0100

    Refactors Resource testing with mock client helper

--- tests/Patreon/Unit/Resources/CurrentUserTest.php
+++ tests/Patreon/Unit/Resources/CurrentUserTest.php
@@ -2,31 +2,18 @@
 
 namespace Squid\Patreon\Tests\Unit\Resources;
 
-use Http\Mock\Client as MockHttpClient;
-use Psr\Http\Message\ResponseInterface;
-use Squid\Patreon\Api\Client;
-use Squid\Patreon\Entities\Entity;
+use Squid\Patreon\Entities\User;
 use Squid\Patreon\Resources\CurrentUser;
 use Squid\Patreon\Tests\Unit\TestCase;
-use WoohooLabs\Yang\JsonApi\Schema\Document;
-use WoohooLabs\Yang\JsonApi\Response\JsonApiResponse;
 
 class CurrentUserTest extends TestCase
 {
     public function testMeReturnsHydratedUserEntity(): void
     {
-        $document = $this->createMock(JsonApiResponse::class);
-        $document->expects($this->once())
-            ->method('document')
-            ->willReturn(Document::createFromArray(['data' => ['type' => 'user']]));
-
-        $client = $this->createMock(Client::class);
-        $client->expects($this->once())
-            ->method('get')
-            ->willReturn($document);
+        $client = $this->createClientMockForResource('user');
 
         $user = (new CurrentUser($client))->get();
 
-        $this->assertInstanceOf(Entity::class, $user);
+        $this->assertInstanceOf(User::class, $user);
     }
 }
--- tests/Patreon/Unit/TestCase.php
+++ tests/Patreon/Unit/TestCase.php
@@ -2,7 +2,11 @@
 
 namespace Squid\Patreon\Tests\Unit;
 
+use Http\Mock\Client as MockHttpClient;
 use PHPUnit\Framework\TestCase as PHPUnit_TestCase;
+use Squid\Patreon\Api\Client;
+use WoohooLabs\Yang\JsonApi\Schema\Document;
+use WoohooLabs\Yang\JsonApi\Response\JsonApiResponse;
 
 class TestCase extends PHPUnit_TestCase
 {
@@ -16,5 +20,27 @@ class TestCase extends PHPUnit_TestCase
     public function fixture(string $path): string
     {
         return file_get_contents(__DIR__ . '/../fixtures/' . $path);
+    }
+
+    /**
+     * Returns a mock Client for a JSON Api Resource of type.
+     *
+     * @param string $type Type of resource
+     *
+     * @return Squid\Patreon\Api\Client
+     */
+    protected function createClientMockForResource(string $type): Client
+    {
+        $document = $this->createMock(JsonApiResponse::class);
+        $document->expects($this->once())
+            ->method('document')
+            ->willReturn(Document::createFromArray(['data' => ['type' => $type]]));
+
+        $client = $this->createMock(Client::class);
+        $client->expects($this->once())
+            ->method('get')
+            ->willReturn($document);
+
+        return $client;
     }
 }
