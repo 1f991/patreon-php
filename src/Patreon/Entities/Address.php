ce8e3c1f474fd9c5746b9ae83300023798a5e898
Samuel Ryan <sam@samryan.co.uk>
2018-04-10 16:54:40 +0100

    Adds Address entity

---
+++ src/Patreon/Entities/Address.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Squid\Patreon\Entities;
+
+class Address extends Entity
+{
+    /**
+    * Name of the person to deliver to at this address.
+    * Example: John Doe
+    *
+    * @var
+    */
+    public $addressee;
+
+    /**
+    * Name of the city.
+    * Example: London
+    *
+    * @var string
+    */
+    public $city;
+
+    /**
+    * ISO 3166-1 Alpha-2 Code representing the country.
+    * Example: GB
+    *
+    * @var string
+    */
+    public $country;
+
+    /**
+    * First line of the street address.
+    * Example: Aldgate Tower
+    *
+    * @var string
+    */
+    public $line_1;
+
+    /**
+    * Second line of the street address.
+    * Example: 2 Leman Street
+    *
+    * @var string
+    */
+    public $line_2;
+
+    /**
+    * Postal / Zip Code of the street address.
+    * Example: E1 8FA
+    *
+    * @var string
+    */
+    public $postal_code;
+
+    /**
+    * ANSI standard INCITS 38:2009 US State code if address is in the United
+    * States.
+    * Example: CA
+    *
+    * @var string
+    */
+    public $state;
+}
---
+++ tests/Patreon/Unit/Entities/AddressTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Squid\Patreon\Tests\Unit\Entities;
+
+use Squid\Patreon\Entities\Address;
+
+class AddressTest extends TestCase
+{
+    public function testGoalEntityHasAllRequiredProperties()
+    {
+        $schema = [
+            'addressee',
+            'city',
+            'country',
+            'line_1',
+            'line_2',
+            'postal_code',
+            'state',
+        ];
+
+        $this->assertTrue($this->validateEntitySchema(new Address, $schema));
+    }
+}
