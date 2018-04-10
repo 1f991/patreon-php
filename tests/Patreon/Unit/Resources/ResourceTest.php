dd110a3d5940594fb94fbf9a2aba54cb499d3200
Samuel Ryan <sam@samryan.co.uk>
2018-04-10 19:56:04 +0100

    Throws Exceptions when API returns errors (#5)

---
+++ tests/Patreon/Unit/Resources/ResourceTest.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Squid\Patreon\Tests\Unit\Resources;
+
+use Exception;
+use Squid\Patreon\Api\Client;
+use Squid\Patreon\Entities\User;
+use Squid\Patreon\Exceptions\UnauthorizedException;
+use Squid\Patreon\Resources\Resource;
+use Squid\Patreon\Tests\Unit\TestCase;
+use WoohooLabs\Yang\JsonApi\Schema\Document;
+use WoohooLabs\Yang\JsonApi\Response\JsonApiResponse;
+
+class ResourceTest extends TestCase
+{
+    public function testResourceThrowsExceptionWhenApiReturnsError(): void
+    {
+        $document = Document::createFromArray(
+            [
+            'errors' => [
+                ['code_name' => '', 'detail' => 'example'],
+            ],
+            ]
+        );
+
+        $response = $this->createMock(JsonApiResponse::class);
+        $response->method('document')->willReturn($document);
+
+        $client = $this->createMock(Client::class);
+
+        $this->expectException(Exception::class);
+
+        $user = (new ResourceExample($client))->get($response);
+    }
+}
+
+class ResourceExample extends Resource
+{
+    public function get(JsonApiResponse $response): void
+    {
+        $this->hydrateJsonApiResponse($response);
+    }
+}
