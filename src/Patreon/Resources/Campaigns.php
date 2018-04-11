91819f6bcc2cf167d68f620b9567dfe79428de26
Samuel Ryan <sam@samryan.co.uk>
2018-04-11 23:16:24 +0100

    Adds methods for getting Campaigns with Pledges

--- src/Patreon/Resources/Campaigns.php
+++ src/Patreon/Resources/Campaigns.php
@@ -17,4 +17,64 @@ class Campaigns extends Resource
             $this->client->get('current_user/campaigns')->document()
         )->first();
     }
+
+    /**
+     * Get the current users Campaign with Pledges.
+     *
+     * @return \Squid\Patreon\Entities\Campaign
+     */
+    public function getMyCampaignWithPledges(): Campaign
+    {
+        $campaign = $this->hydrateDocument(
+            $this->client->get('current_user/campaigns')->document()
+        )->first();
+
+        return $this->attachPledges($campaign);
+    }
+
+    /**
+     * Get a Campaign by ID.
+     *
+     * @param integer $id Campaign ID
+     *
+     * @return \Squid\Patreon\Entities\Campaign
+     */
+    public function getCampaign(int $id): Campaign
+    {
+        return $this->hydrateDocument(
+            $this->client->get("campaigns/{$id}")->document()
+        )->first();
+    }
+
+    /**
+     * Get a Campaign by ID with Pledges.
+     *
+     * @param integer $id Campaign ID
+     *
+     * @return \Squid\Patreon\Entities\Campaign
+     */
+    public function getCampaignWithPledges(int $id): Campaign
+    {
+        $campaign = $this->hydrateDocument(
+            $this->client->get("campaigns/{$id}")->document()
+        )->first();
+
+        return $this->attachPledges($campaign);
+    }
+
+    /**
+     * Retrieves and attaches pledges for a campaign to a Campaign object.
+     *
+     * @param Campaign $campaign Campaign entity to fetch pledges for and attach to
+     *
+     * @return \Squid\Patreon\Entities\Campaign
+     */
+    protected function attachPledges($campaign): Campaign
+    {
+        $pledges = (new Pledges($this->client))->getCampaignPledges($campaign->id);
+
+        $campaign->pledges = $pledges;
+
+        return $campaign;
+    }
 }
