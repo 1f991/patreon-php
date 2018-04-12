052240070141021ca8bd53752ff7fecabc9dae1d
Samuel Ryan <sam@samryan.co.uk>
2018-04-12 17:39:29 +0100

    Adds ResourceHasNoEntity Exception to Entity Hydrator

---
+++ src/Patreon/Exceptions/ResourceHasNoEntity.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Squid\Patreon\Exceptions;
+
+use LogicException;
+
+class ResourceHasNoEntity extends LogicException
+{
+    /**
+     * Provides an erorr for when a Resource does not have an associated Entity.
+     *
+     * @param string $name Name of the Resource.
+     *
+     * @return \Squid\Patreon\Exceptions\ResourceHasNoEntity
+     */
+    public static function forResource(string $name): self
+    {
+        $message = "No entity is configured for {$name} resources.";
+
+        return new static($message);
+    }
+}
